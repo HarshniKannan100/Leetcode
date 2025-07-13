db7d580e2b463a33835f0eb782c6b1a34d4682d7
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2025-07-13 14:30:53 +0530

    Time: 26 ms (66.18%) | Memory: 57.8 MB (62.33%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,22 +1,14 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Queue<Integer> p=new PriorityQueue<>(Collections.reverseOrder());
-        Queue<Integer> t=new PriorityQueue<>(Collections.reverseOrder()); 
-        for (int i=0;i<players.length;i++){
-            p.add(players[i]);
-        }     
-        for(int i=0;i<trainers.length;i++){
-            t.add(trainers[i]);
-        }
-        int count=0;
-        while(!p.isEmpty() && !t.isEmpty()){
-            if(p.element()<=t.element()){
-                p.poll();
-                t.poll();
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int p=0,t=0,count=0;
+        while(p<players.length && t<trainers.length){
+            if(trainers[t]>=players[p]){
                 count++;
-            }else{
-                p.poll();
+                p++;
             }
+            t++;
         }
         return count;
     }
