d07f108f605de41d93d277407c80bccfb6894b0c
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2025-12-01 22:01:14 +0530

    Time: 27 ms (20.88%) | Memory: 72.7 MB (45.05%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
@@ -3,12 +3,12 @@ class Solution {
         Arrays.sort(batteries);
         long total=0;
         for(int b:batteries) total+=b;
-        int i=batteries.length-1,k=0;
-        while(batteries[i]>(total/(n-k))){
-            k++;
+        int i=batteries.length-1;
+        while(batteries[i]>(total/(n))){
             total-=batteries[i];
             i--;
+            n--;
         }
-        return total/(n-k);
+        return total/(n);
     }
 }
