12d3b94bcb35dab67e51d5c521b9caf2c154d550
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2025-12-15 19:05:36 +0530

    Time: 2 ms (100.00%) | Memory: 84.3 MB (31.85%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -1,26 +1,19 @@
 class Solution {
-    long func(long n){
-        long val=1,inc=2;
-        for(int i=1;i<n;i++){
-            val+=inc;
-            inc++;
-        }
-        return val;
-    }
     public long getDescentPeriods(int[] prices) {
-        long tot=0,count=1;
-        int idx=1,val=0;
-        while(idx<prices.length){
-            if(prices[idx]+1==prices[idx-1]){
-                while(idx<prices.length && prices[idx]+1==prices[idx-1]){
-                    count++;
-                    idx++;
-                }
-                tot+=func(count);
-                val+=count;
-                count=1;
-            }else idx++;
+        long ans = 1;
+        int n = prices.length;
+        int smooth = 1;
+
+        for(int i = 1; i < n; i++){
+            if(prices[i-1]-prices[i] == 1){
+                smooth++;
+            }else{
+                smooth = 1;
+            }
+
+            ans += smooth;
         }
-        return tot+(prices.length-val);
+
+        return ans;
     }
 }
