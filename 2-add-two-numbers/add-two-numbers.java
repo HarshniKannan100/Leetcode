7e3f0b191801c8a8f2ee7815b7eb3e29305d3a45
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2026-01-30 23:58:59 +0530

    Time: 8 ms (2.80%) | Memory: 47.3 MB (5.45%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -8,26 +8,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+import java.math.BigInteger;
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode();
-        ListNode res = dummy;
-        int total = 0, carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            total = carry;
-            if (l1 != null) {
-                total += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                total += l2.val;
-                l2 = l2.next;
-            }
-            int num = total % 10;
-            carry = total / 10;
-            dummy.next = new ListNode(num);
-            dummy = dummy.next;
+    BigInteger revSum(ListNode l){
+        BigInteger sum=BigInteger.ZERO;
+        BigInteger m=BigInteger.ONE;
+        while(l!=null){
+            sum=sum.add(BigInteger.valueOf(l.val).multiply(m));
+            m=m.multiply(BigInteger.TEN);
+            l=l.next;
         }
-        return res.next;        
+        return sum;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        BigInteger sum=BigInteger.ZERO;
+        sum=revSum(l1).add(revSum(l2));
+
+        int mod=sum.mod(BigInteger.TEN).intValue();
+        ListNode l=new ListNode(mod);
+        sum=sum.divide(BigInteger.TEN);
+        ListNode head=l;
+        while(sum.compareTo(BigInteger.ZERO)>0){
+            mod=sum.mod(BigInteger.TEN).intValue();
+            l.next=new ListNode(mod);
+            sum=sum.divide(BigInteger.TEN);
+            l=l.next;
+        }
+        return head;
     }
 }
