5e5d06ba034523a0fc9178280b5502864b2b288b
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2026-02-14 23:40:32 +0530

    Time: 3 ms (76.33%) | Memory: 48.6 MB (9.54%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double dp[][]=new double[102][102];
+        dp[0][0]=(double)poured;
+        for(int r=0;r<=query_row;r++){
+            for(int c=0;c<=r;c++){
+                if(dp[r][c]>1.0){
+                    double excess=(dp[r][c]-1.0)/2.0;
+                    dp[r][c]=1.0;
+                    dp[r+1][c]+=excess;
+                    dp[r+1][c+1]+=excess;
+              }
+            }
+        }
+        return dp[query_row][query_glass];
+    }
+}
