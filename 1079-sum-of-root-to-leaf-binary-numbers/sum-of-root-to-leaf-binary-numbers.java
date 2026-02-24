d4e491e4846a25f5570f161250a8e6b65d3d94d0
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2026-02-24 15:18:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (28.76%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -14,30 +14,17 @@
  * }
  */
 class Solution { 
-    int bit(String s){
-        int v=0;
-        for(int i=0;i<s.length();i++){
-            v|=((s.charAt(i)=='1')?1:0);
-            v<<=1;
-        }
-        v>>=1;
-        System.out.println(v);
-        return v;
-    }
-    int dfs(TreeNode node,StringBuilder sb){
+    int dfs(TreeNode node,int v){
         if(node==null) return 0;
-        sb.append(node.val);
-        int sum=0;
+        v<<=1;
+            v|=node.val;
         if(node.left==null && node.right==null){
-            sum=bit(sb.toString());
-        }else{
-            sum=dfs(node.left,sb)+dfs(node.right,sb);
+            return v;
         }
-        sb.deleteCharAt(sb.length()-1);
-        return sum;
+        return dfs(node.left,v)+dfs(node.right,v);
     }
     public int sumRootToLeaf(TreeNode root) {
         StringBuilder sb=new StringBuilder();
-        return dfs(root,sb);
+        return dfs(root,0);
     }
 }
