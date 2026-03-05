b1f28a3510f8e5c112cdc08c8abb156076b92e01
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2026-03-05 08:32:33 +0530

    Time: 4 ms (75.73%) | Memory: 43.5 MB (95.87%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -3,14 +3,8 @@ class Solution {
         int st=0,c=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)-'0'!=st) c++;
-            st=st==0?1:0;
+            st^=1;
         }
-        st=1;
-        int c1=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)-'0'!=st) c1++;
-            st=st==0?1:0;
-        }
-        return Math.min(c1,c);
+        return Math.min(c,s.length()-c);
     }
 }
