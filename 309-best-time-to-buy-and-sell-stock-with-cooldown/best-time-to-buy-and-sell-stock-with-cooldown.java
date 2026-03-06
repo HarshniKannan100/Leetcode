c413de088c2308316bab3a923b7ace25b035ffc9
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2026-03-06 22:23:50 +0530

    Time: 1 ms (70.87%) | Memory: 43.4 MB (77.15%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,17 +3,12 @@ class Solution {
         int n=prices.length;
         int[][] dp=new int[n+1][2];
         for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                if(j==0){
-                    int buy=dp[i+1][0]-prices[i];
-                    int cdown=dp[i+1][1];
-                    dp[i][1]=Math.max(buy,cdown);
-                }else{
-                    int sell=(i+2<n)?dp[i+2][1]+prices[i]:prices[i];
-                    int cdown=dp[i+1][0];
-                    dp[i][0]=Math.max(sell,cdown);
-                }
-            }
+            int buy=dp[i+1][0]-prices[i];
+            int cdown=dp[i+1][1];
+            dp[i][1]=Math.max(buy,cdown);
+            int sell=(i+2<n)?dp[i+2][1]+prices[i]:prices[i];
+            cdown=dp[i+1][0];
+            dp[i][0]=Math.max(sell,cdown);
         }
         return dp[0][1];
     }
