e1ee4639d9e58940c2ee543a35abe4731005fb01
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2026-03-26 08:46:56 +0530

    Time: 5 ms (46.85%) | Memory: 161.3 MB (48.25%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,51 +1,26 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
+        long tot=0;
         int R=grid.length,C=grid[0].length;
-        long[] rowPre=new long[R];
-        long[] rowSuf=new long[R];
-        long[] colPre=new long[C];
-        long[] colSuf=new long[C];
-        long prev=0;
         for(int i=0;i<R;i++){
-            long sum=0;
+            for(int j=0;j<C;j++){
+                tot+=grid[i][j];
+            }
+        }
+        if(tot%2!=0) return false;
+        long sum=0;
+        for(int i=0;i<R;i++){
             for(int j=0;j<C;j++){
                 sum+=grid[i][j];
             }
-            rowPre[i]=sum+prev;
-            prev=rowPre[i];
+            if(tot/2==sum) return true;
         }
-        prev=0;
-        for(int i=R-1;i>=0;i--){
-            long sum=0;
-            for(int j=0;j<C;j++){
-                sum+=grid[i][j];
-            }
-            rowSuf[i]=sum+prev;
-            prev=rowSuf[i];
-        }
-        prev=0;
+        sum=0;
         for(int j=0;j<C;j++){
-            long sum=0;
             for(int i=0;i<R;i++){
                 sum+=grid[i][j];
             }
-            colPre[j]=sum+prev;
-            prev=colPre[j];
-        }
-        prev=0;
-        for(int j=C-1;j>=0;j--){
-            long sum=0;
-            for(int i=0;i<R;i++){
-                sum+=grid[i][j];
-            }
-            colSuf[j]=sum+prev;
-            prev=colSuf[j];
-        }
-        for(int i=0;i<R-1;i++){
-            if(rowPre[i]==rowSuf[i+1]) return true;
-        }
-        for(int i=0;i<C-1;i++){
-            if(colPre[i]==colSuf[i+1]) return true;
+            if(tot/2==sum) return true;
         }
         return false;
     }
