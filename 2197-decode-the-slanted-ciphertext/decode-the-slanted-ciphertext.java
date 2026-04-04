0f7e9e7087a78676f9fc69b64d7ad2d9ebfb0e76
Harshni Kannan <162335134+HarshniKannan100@users.noreply.github.com>
2026-04-04 12:29:25 +0530

    Time: 54 ms (8.28%) | Memory: 57.7 MB (76.92%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,17 +1,13 @@
 class Solution {
     public String decodeCiphertext(String encodedText, int rows) {
         int C=encodedText.length()/rows;
-        char[][] mat=new char[rows][C];
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<C;j++){
-                if((i*C)+j+i>=encodedText.length()) break;
-                mat[i][j]=encodedText.charAt((i*C)+j+i);
-            }
-        }
         StringBuilder sb=new StringBuilder();
         for(int j=0;j<C;j++){
             for(int i=0;i<rows;i++){
-                sb.append(mat[i][j]);
+                if(((i*C)+j+i)>=encodedText.length()){
+                    break;
+                }
+                sb.append(encodedText.charAt((i*C)+j+i));
             }
         }
         int r=sb.length()-1;
